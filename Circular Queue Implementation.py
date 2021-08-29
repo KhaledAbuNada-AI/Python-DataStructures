7eeef8c9a49067f065291e5095df706fd333d4d0
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-29 22:35:50 +0300

    Create Function dequeue
    Khaled Abu Nada

--- Circular Queue Implementation.py
+++ Circular Queue Implementation.py
@@ -10,5 +10,32 @@ class CircularQueue():
 
     def enqueue(self, data):
 
+        # condition if queue is full
         if ((self.rear + 1) % self.size == self.front):
             return (" Queue is Full\n")
+
+        # condition for empty queue
+        elif self.front == -1:
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = data
+        else:
+
+            # next position of rear
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = data
+
+    def dequeue(self):
+        if self.front == -1:
+            return "Queue is Empty\n"
+
+        elif self.front == self.rear:
+            temp = self.queue[self.front]
+            self.front = -1
+            self.rear = -1
+            return temp
+
+        else:
+            temp = self.queue[self.front]
+            self.front = (self.front + 1) % self.size
+            return temp
